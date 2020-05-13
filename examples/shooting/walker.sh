bb756ce6f5e0ae51aff29fbba7c7f0272b04e3ce
Ian Abraham <iabr4073@gmail.com>
2020-05-13 00:04:02 -0500

    added walker en

--- examples/shooting/walker.sh
+++ examples/shooting/walker.sh
@@ -5,12 +5,12 @@ do
     python3 main.py \
                 --env "Walker2DEnv" \
                 --max_steps 2000 \
-                --max_frames 80000 \
-		        --method 'mppi' \
+                --max_frames 20000 \
+		        --method 'shooting' \
                 --horizon 10 \
                 --frame_skip 4 \
                 --seed $seed \
-		        --log \
+                --log \
                 --no_render
     echo "trial $seed"
 done
